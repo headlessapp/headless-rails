b9d576993d0a1a32872ca4d04882ad8c6f013929
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2013-08-16 01:03:07 +0100

    cleaner way to build Rack Requests

--- spec/headless/rails/escaped_fragment_extractor_spec.rb
+++ spec/headless/rails/escaped_fragment_extractor_spec.rb
@@ -5,20 +5,33 @@ RSpec.describe Headless::Rails::EscapedFragmentExtractor do
 
   describe "call" do
 
+    let(:request) { request_for_uri(uri) }
+
+    subject do
+      described_class.call(request)
+    end
+
     context "with no escaped_fragment" do
-      subject do
-        described_class.call(example_request)
-      end
+
+      let(:uri) { "/user/matthewrudy" }
 
       it "returns a full url" do
         expect(subject).to eq "http://localhost:3000/user/matthewrudy"
       end
     end
 
-    context "with an empty escaped_fragment" do
-      subject do
-        described_class.call(example_request_with_fragment(""))
+    context "with some params" do
+
+      let(:uri) { "/user/matthewrudy?foo=bar" }
+
+      it "keeps the params intact" do
+        expect(subject).to eq "http://localhost:3000/user/matthewrudy?foo=bar"
       end
+    end
+
+    context "with an empty escaped_fragment" do
+
+      let(:uri) { "/user/matthewrudy?_escaped_fragment_=" }
 
       it "returns a full url with the escaped fragment removed" do
         expect(subject).to eq "http://localhost:3000/user/matthewrudy"
@@ -26,12 +39,20 @@ RSpec.describe Headless::Rails::EscapedFragmentExtractor do
     end
 
     context "with an escaped_fragment" do
-      subject do
-        described_class.call(example_request_with_fragment("/show/twitter"))
-      end
+
+      let(:uri) { "/user/matthewrudy?_escaped_fragment_=/show/twitter" }
 
       it "returns the full url with an anchor appended" do
         expect(subject).to eq "http://localhost:3000/user/matthewrudy#/show/twitter"
+      end
+    end
+
+    context "with an escaped_fragment and some params" do
+
+      let(:uri) { "/user/matthewrudy?foo=bar&_escaped_fragment_=/show/twitter" }
+
+      it "returns the full url with an anchor appended" do
+        expect(subject).to eq "http://localhost:3000/user/matthewrudy?foo=bar#/show/twitter"
       end
     end
   end
--- spec/support/example_requests.rb
+++ spec/support/example_requests.rb
@@ -1,43 +1,44 @@
 module ExampleRequests
 
-  def example_request_env
+  def request_env_for_uri(uri)
+    path, query = uri.split("?")
+
     {
-      "SERVER_SOFTWARE"=>"thin 1.5.1 codename Straight Razor",
-      "SERVER_NAME"=>"localhost",
-      "REQUEST_METHOD"=>"GET",
-      "REQUEST_PATH"=>"/user/matthewrudy",
-      "PATH_INFO"=>"/user/matthewrudy",
-      "REQUEST_URI"=>"/user/matthewrudy",
-      "HTTP_VERSION"=>"HTTP/1.1",
-      "HTTP_HOST"=>"localhost:3000",
-      "HTTP_CONNECTION"=>"keep-alive",
-      "HTTP_ACCEPT"=>"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-      "HTTP_USER_AGENT"=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36",
-      "HTTP_ACCEPT_ENCODING"=>"gzip,deflate,sdch",
-      "HTTP_ACCEPT_LANGUAGE"=>"en-GB,en;q=0.8,en-US;q=0.6,zh;q=0.4",
-      "HTTP_COOKIE"=>"foo=bar",
-      "GATEWAY_INTERFACE"=>"CGI/1.2",
-      "SERVER_PORT"=>"3000",
-      "QUERY_STRING"=>"",
-      "SERVER_PROTOCOL"=>"HTTP/1.1",
-      "rack.url_scheme"=>"http",
-      "SCRIPT_NAME"=>"",
-      "REMOTE_ADDR"=>"127.0.0.1",
+      "SERVER_SOFTWARE"      => "thin 1.5.1 codename Straight Razor",
+      "HTTP_VERSION"         => "HTTP/1.1",
+      "HTTP_CONNECTION"      => "keep-alive",
+      "HTTP_ACCEPT"          => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+      "HTTP_USER_AGENT"      => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36",
+      "HTTP_ACCEPT_ENCODING" => "gzip,deflate,sdch",
+      "HTTP_ACCEPT_LANGUAGE" => "en-GB,en;q=0.8,en-US;q=0.6,zh;q=0.4",
+      "HTTP_COOKIE"          => "foo=bar",
+      "GATEWAY_INTERFACE"    => "CGI/1.2",
+
+      "SCRIPT_NAME" => "",
+      "REMOTE_ADDR" => "127.0.0.1",
+
+      # http method
+      "REQUEST_METHOD" => "GET",
+
+      # protocol
+      "SERVER_PROTOCOL" => "HTTP/1.1",
+      "rack.url_scheme" => "http",
+
+      # host and port
+      "SERVER_NAME" => "localhost",
+      "HTTP_HOST"   => "localhost:3000",
+      "SERVER_PORT" => "3000",
+
+      # path and query string
+      "REQUEST_PATH" => path,
+      "PATH_INFO"    => path,
+      "REQUEST_URI"  => uri,
+      "QUERY_STRING" => query,
     }
   end
 
-  def example_request_env_with_fragment(escaped_fragment)
-    example_request_env.merge(
-      "QUERY_STRING"=>"_escaped_fragment_=#{escaped_fragment}",
-      "REQUEST_URI"=>"/user/matthewrudy?_escaped_fragment_=#{escaped_fragment}"
-    )
+  def request_for_uri(uri)
+    ::Rack::Request.new(request_env_for_uri(uri))
   end
 
-  def example_request
-    ::Rack::Request.new(example_request_env)
-  end
-
-  def example_request_with_fragment(escaped_fragment)
-    ::Rack::Request.new(example_request_env_with_fragment(escaped_fragment))
-  end
 end
